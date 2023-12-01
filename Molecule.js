f929d9bb1a4e1fdc2770a52a61403b895d2566a6
lereja1 <151942719+lereja1@users.noreply.github.com>
2023-12-01 11:30:50 -0500

    V.2

--- Molecule.js
+++ Molecule.js
@@ -1,24 +1,61 @@
+let MoleculeTypes = {
+    Eau: {
+        mass:18,
+        color: [0,100,255]
+    },
+    //pour chercher ex: eau masse moleculaire
+    //copier pour ajouter
+    // Eau: {
+    //     mass:18,
+    //     color: [0,100,255]
+    // },
+    Air: {
+        mass:29,
+        color: [200,200,255]
+    },
+    Mercure: {
+        mass:200,
+        color: [200,200,255]
+    },
+    Carbone: {
+        mass:12,
+        color: [80,60,90]
+    },
+    Argon: {
+        mass:40,
+        color: [200,180,40]
+        },
+}
+
 class Molecule {
-    constructor(x,y,i) {
+    constructor(x,y, vx, vy, type ,i) {
         this.i = i;
 
         this.x = x;
         this.y = y;
 
-        this.vx =  random(-3, 3);
-        this.vy =  random(-3, 3);
+        this.vx =  vx;
+        this.vy =  vy;
         
-        this.mass = random(7, 19);
+        this.type = type;
+
+        this.mass = MoleculeTypes[type].mass
     
         this.color = this.selectColor();
+
+        this.size = map(this.mass, 0, 200, 10, 40)
     }
 
     selectColor() {
         return color(
-            random(0, 255),
-            random(0, 255),
-            random(0, 255)
+            MoleculeTypes[this.type].color[0],
+            MoleculeTypes[this.type].color[1],
+            MoleculeTypes[this.type].color[2]
         );
+        //     random(0, 255),
+        //     random(0, 255),
+        //     random(0, 255)
+        //);
     }
 
 
@@ -27,16 +64,22 @@ class Molecule {
 
         noStroke();
         fill(this.color);
-        circle(this.x, this.y, this.mass)
+        circle(this.x, this.y, this.size);
 
         if (DETAILS){
+            stroke(0);
+            let v = createVector(this.vx, this.vy)
+            v.normalize();
+
+            line(this.x,this.y,5 * TEMPERATURE * v.x + this.x,5 * TEMPERATURE * v.y + this.y);
             
         fill(150)
         textSize(10);
         text(
             
             "ID: " + this.i + "\n" +
-            "Mass: " + this.mass.toFixed(2) + "Kg\n",
+            "Type: " + this.type + "\n" +
+            "Mass: " + this.mass.toFixed(2) + "u\n",
             this.x-15,
             this.y-30
         );
@@ -48,11 +91,11 @@ class Molecule {
         this.y = this.y + this.vy * TEMPERATURE;
 
         if (this.x > width || this.x < 0){
-            this.vx = -this.vx
+            this.vx = -this.vx * 0.95
         }
 
         if (this.y > height || this.y < 0){
-            this.vy = -this.vy
+            this.vy = -this.vy * 0.95
         }
         this.x = min(max(this.x, 0), width);
         this.y = min(max(this.y, 0), height);
@@ -68,11 +111,21 @@ class Molecule {
                 molecules[i].x,
                 molecules[i].y
             );
-            
-            if (d < this.mass/2 + molecules[i].mass/2) {
-                this.vx = -this.vx;
-                this.vy = -this.vy;
+        
+            if (d < this.size/2 + molecules[i].size/2) {
 
+                let sumX = this.vx + molecules[i].vx;
+                let sumY = this.vy + molecules[i].vy;
+
+                
+                this.vx = -sumX/2;
+                this.vy = -sumY/2;
+
+                molecules[i].vx = sumX/2;
+                molecules[i].vy = sumY/2;
+
+                this.move();
+                this.move();
                 this.move();
 
                 this.color = this.selectColor();
--- main.js
+++ main.js
@@ -1,11 +1,18 @@
 let molecules = [];
-let N = 100;
+let N = 0;
+let espaceBouton = 50;
 
-let DETAILS = false;
+let DETAILS = true;
 let button;
 
 let TEMPERATURE = 0.5
 let slider;
+
+let prevMouseX = 0;
+let prevMouseY = 0;
+
+let isDragging = false;
+let currentType = "Eau"
 
 function setup() {
     createCanvas(
@@ -18,6 +25,16 @@ function setup() {
             DETAILS = !DETAILS;
         })
 
+        console.log(MoleculeTypes);
+
+        for (let typeName in MoleculeTypes) {
+        createButton(typeName).mousePressed(() => {
+            
+            currentType = typeName;
+        });
+        }
+
+
     slider = createSlider(0.1 ,5, 0.1,0.01)
     slider.size(width-110);
 
@@ -25,12 +42,14 @@ function setup() {
         molecules[i] = new Molecule(
             random(0, width),
             random(0, height),
+            random(-3, 3),
+            random(-3, 3),
             i
         );
-
     }
 
-    console.log(molecules);
+    // molecules[0] = new Molecule(400, 400, 10, 10, 0);
+    // molecules[1] = new Molecule(500, 500, -10, -10 ,1);
 }
 
 function draw() {
@@ -38,11 +57,62 @@ function draw() {
 
     TEMPERATURE = slider.value();
     //dessiner chaque molecule
-    for (let i = 0; i < N; i++) {
+    for (let i = 0; i < molecules.length; i++) {
         molecules[i].draw();
         molecules[i].move();
 
         molecules[i].collision(molecules);
 
+
     }
+
+    if (isDragging) {
+        stroke(0);
+        line(prevMouseX, prevMouseY, mouseX, mouseY)
+    }
+
+}
+
+
+function mousePressed() { 
+    console.log(mouseX, mouseY);
+
+    prevMouseX = mouseX
+
+    prevMouseY = mouseY
+
+
+}
+
+function mouseDragged(){
+
+    if (mouseY > height - espaceBouton) return
+    if (prevMouseY > height - espaceBouton) return
+
+    isDragging = true;
+
+    
+
+
+}
+
+
+function mouseReleased(){
+    isDragging = false;
+                            //peut changer
+    if (prevMouseY > height - espaceBouton) return
+
+    let dx = prevMouseX - mouseX;
+    let dy = prevMouseY - mouseY;
+
+    molecules.push(
+        new Molecule(
+            prevMouseX,
+            prevMouseY,
+            dx * 0.1,
+            dy * 0.1,
+            currentType,
+            molecules.length
+            )
+        );
 }
